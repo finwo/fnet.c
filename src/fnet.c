2c0ae0ae4a70c9946ff4d03d716743d8760e4d56
Yersa Nordman <yersa@finwo.nl>
2023-07-24 04:05:54 +0200

    More argument presence checking

--- src/fnet.c
+++ src/fnet.c
@@ -48,22 +48,64 @@ struct fnet_t * fnet_listen(const char *address, uint16_t port, struct fnet_conn
 }
 
 struct fnet_t * fnet_connect(const char *address, struct fnet_connect_options_t *options) {
+
+  // Checking arguments are given
+  if (!address) {
+    fprintf(stderr, "fnet_connect: address argument is required\n");
+    return NULL;
+  }
+  if (!options) {
+    fprintf(stderr, "fnet_connect: options argument is required\n");
+    return NULL;
+  }
+
   return NULL;
 }
 
 void fnet_process(struct fnet_t *connection) {
   struct fnet_internal_t *conn = (struct fnet_internal_t *)connection;
+
+  // Checking arguments are given
+  if (!conn) {
+    fprintf(stderr, "fnet_process: connection argument is required\n");
+    return NULL;
+  }
+
 }
 void fnet_write(struct fnet_t *connection, struct buf *buf) {
   struct fnet_internal_t *conn = (struct fnet_internal_t *)connection;
+
+  // Checking arguments are given
+  if (!conn) {
+    fprintf(stderr, "fnet_write: connection argument is required\n");
+    return NULL;
+  }
+  if (!buf) {
+    fprintf(stderr, "fnet_write: buf argument is required\n");
+    return NULL;
+  }
+
 }
 
 void fnet_close(struct fnet_t *connection) {
   struct fnet_internal_t *conn = (struct fnet_internal_t *)connection;
+
+  // Checking arguments are given
+  if (!conn) {
+    fprintf(stderr, "fnet_close: connection argument is required\n");
+    return NULL;
+  }
 }
 
 void fnet_free(struct fnet_t *connection) {
   struct fnet_internal_t *conn = (struct fnet_internal_t *)connection;
+
+  // Checking arguments are given
+  if (!conn) {
+    fprintf(stderr, "fnet_free: connection argument is required\n");
+    return NULL;
+  }
+
   free(conn);
 }
 
